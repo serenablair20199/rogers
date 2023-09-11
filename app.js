eb5fa2a6988eae16b2645f685f37cb05b6162b4b
serenablair20199 <144683184+serenablair20199@users.noreply.github.com>
2023-09-11 07:12:30 -0400

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_83yvd0m", "template_z1yh0dq", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
